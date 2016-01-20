320937fe51720d5e7e5c8588253337a0231a303d
Anway Kulkarni <anway.kulkarni@iauro.com>
2016-01-20 13:12:23 +0530

    Program for hello word in C

---
+++ helloWord.c
@@ -0,0 +1,15 @@
+/**
+    helloWord.c
+    Purpose: Hello Word in C
+    @author Anway Kulkarni
+*/
+#include<stdio.h>
+
+int main()
+{
+    //pritnf statement is used for priting the string on console
+    //%s is used for the strings in C
+    printf("%s","Hello Word\n");
+    return 0;
+}
+
