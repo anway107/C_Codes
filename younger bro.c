059c4ba8e08563c77c3e85d3b7700064f0d58360
Ganesh Deshmukh <Ganesh Deshmukh>
2016-01-30 21:40:56 +0530

    changes

--- younger bro.c
+++ younger bro.c
@@ -1,18 +1,29 @@
+/**
+    younger bro.c
+    Purpose: comparision of brothers in C
+    @author Anway Kulkarni
+    @author Sanket Deshpande
+    @author Mahesh Deshmukh
+*/
 # include<stdio.h>
 
 int main()
 {
+	//Declaration of the variables
 	int Ram_age,Shyam_age,Ajay_age;
 	
+	//printf is used for priting on the console
+    //scanf is used for taking the input from console
 	printf("Enter Ram age");
 	scanf("%d",&Ram_age);
 	
-		printf("Enter Shyam age");
+	printf("Enter Shyam age");
 	scanf("%d",&Shyam_age);
 	
-		printf("Enter Ajay age");
+	printf("Enter Ajay age");
 	scanf("%d",&Ajay_age);
 	
+	//condition of comparision of ages
 	if(Ram_age>Shyam_age)
 	printf("shyam is younger",Shyam_age);
 	
